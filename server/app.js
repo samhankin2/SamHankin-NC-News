69fdef7406f388564532b6c11934a5fb816ceb14
SamHankin <samhankin2>
2019-10-07 16:04:32 +0100

    adding cors

--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 
 const apiRouter = require("./routers/apiRouter");
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use("/api", apiRouter);
